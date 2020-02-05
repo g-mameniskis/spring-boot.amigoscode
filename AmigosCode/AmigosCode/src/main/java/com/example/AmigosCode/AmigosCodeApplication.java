52b03f5139a9e0b4c2e87e9b40a81fcde6a60467
Grayson Mameniskis <57866995+g-mameniskis@users.noreply.github.com>
2020-02-05 14:33:34 -0500

    Add files via upload

---
+++ AmigosCode/AmigosCode/src/main/java/com/example/AmigosCode/AmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmigosCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosCodeApplication.class, args);
+	}
+
+}
---
+++ AmigosCode/AmigosCode/src/test/java/com/example/AmigosCode/AmigosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmigosCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
